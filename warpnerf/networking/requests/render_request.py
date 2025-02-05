f40634d47cee4cbb0657d5a1cef4ca99d4b63848
James <james.perlman@dream-ai.com>
2025-02-04 16:37:31 -0800

    id value in render request

--- warpnerf/networking/requests/render_request.py
+++ warpnerf/networking/requests/render_request.py
@@ -4,14 +4,15 @@ from warpnerf.scene.objects.dict_compatible import DictSerializable
 from warpnerf.scene.objects.perspective_camera import PerspectiveCamera
 from warpnerf.scene.objects.wn_scene import WNScene
 
-
 class RenderRequest(DictSerializable):
+    id: any
     scene: WNScene
     camera: PerspectiveCamera
     size: Tuple[int, int]
 
     def to_dict(self):
         return {
+            "id": self.id,
             "scene": self.scene.to_dict(),
             "camera": self.camera.to_dict(),
             "size": self.size
